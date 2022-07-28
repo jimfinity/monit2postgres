828209dd97e33fc053865f4cc5cb73d6ef015778
jimfinity <quiet.infinity@gmail.com>
2022-07-28 15:31:44 +0100

    added basic postgres insert

--- monit2postgres.py
+++ monit2postgres.py
@@ -4,7 +4,15 @@ import schedule
 from time import sleep
 import requests
 from icecream import ic
+import psycopg2
+import json
 
 response = requests.get('http://192.168.1.21:2812/_status?format=xml')
 monitDict = xmltodict.parse(response.text, dict_constructor=dict, attr_prefix="")
 print (monitDict)
+
+conn = psycopg2.connect("host=test-postgres dbname=testing user=postgres password=mysecretpassword")
+cur = conn.cursor()
+sql = """INSERT INTO test(id, data)
+             VALUES(%s);"""
+cur.execute(sql, (1,json.dumps(monitDict)))
